cc33d5ae22ddcefb1f3b025f93b12366f2f8e125
SabrinaAssya <sabrina.achab@hotmail.com>
2023-01-07 13:02:34 +0100

    seeding part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'rest-client'
+
+puts "Getting Movies data 🎥"
+
+def movies_dataset
+  movies = RestClient.get("https://tmdb.lewagon.com./movie/top_rated")
+  movies_array = JSON.parse(movies)["results"]
+  movies_array.each do |m|
+    Movie.create(
+      title: m["name"],
+      overview: m["overview"],
+      poster_url: m["poster_path"],
+      rating: m["vote_average"]
+    )
+  end
+
+end
+movies_dataset()
+puts "Seeding Movies Data 🌱"
